290e17151c1dfe11060ed8622b030e66f72ca7c2
kwda1002 <77757489+kwda1002@users.noreply.github.com>
2021-04-04 07:57:40 +0900

    Create touch restaurants_searche.php

---
+++ touch restaurants_searche.php
@@ -0,0 +1,26 @@
+# restaurants_seasher.php
+
+<?php
+
+function write_data_to_csv(){
+
+    $restaurants = [];
+    $response = "hogehoge";
+
+    if(isset($response["results"]["error"])){
+      return print("エラーが発生しました！");
+    }
+    
+    if(isset($response["results"]["shop"])){
+        foreach($response["shop"] as &$i){
+            $restaurant_name = $i["name"];
+            $restaurants[] = $restaurant_name;
+        }
+    }
+    
+    return print_r($restaurants);
+}
+
+write_data_to_csv()
+
+?>
